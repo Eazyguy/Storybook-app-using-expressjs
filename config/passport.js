a3f115ed147396818f08f8a7b165a56bc4ed00f3
Israel Johnson <israeljhn0@gmail.com>
2023-12-28 15:39:05 +0100

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ module.exports = (passport)=>{
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL:'/auth/google/callback' || 'https://storybook-qm1j.onrender.com/auth/google/callback'
+        callbackURL:'https://storybook-qm1j.onrender.com/auth/google/callback'
     },
     async (accessToken,refreshToken,profile,done)=>{
         const newUser = {
